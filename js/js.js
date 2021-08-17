017a24e475723bd2da842a91052b6d25daf1b02c
MIN <65863834+kxxrxmxn@users.noreply.github.com>
2021-08-17 13:24:39 +0900

    Update js.js

--- js/js.js
+++ js/js.js
@@ -169,7 +169,7 @@ class App {
   _getWeather(position) {
 
     const api = {
-      key: "YOUR_KEY",
+      key: "2dce14b5cb89113de2ab27e458030969",
       base: "https://api.openweathermap.org/data/2.5/"
     }
 
